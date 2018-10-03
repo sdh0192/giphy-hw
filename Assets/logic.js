22eb61ae1ef92cd60dfa3f70ec4f9f5119fb7201
sdh0192 <sdh0192@live.com>
2018-10-02 21:17:04 -0400

    FINAL COMMIT

--- Assets/logic.js
+++ Assets/logic.js
@@ -9,8 +9,8 @@ var ratingLimit = "PG-13";
 
 //FUNCTION THAT CREATES BUTTONS UPON PAGE LOADING
 //--------------------------------------------------------------------------------------------------------------
-function renderButtons (){
-    for (var i = 0; i < topics.length; i++){
+function renderButtons() {
+    for (var i = 0; i < topics.length; i++) {
         var buttons = $("<button>");
         buttons.addClass("btn");
         buttons.addClass("sportsBtn");
@@ -19,9 +19,9 @@ function renderButtons (){
     }
 
 
-// EMPTY AND RESET GIF CONTAINER
+    // EMPTY AND RESET GIF CONTAINER
     $(".sportsBtn").unbind("click");
-    $(".sportsBtn").on("click", function(){
+    $(".sportsBtn").on("click", function () {
         $(".gif-image").unbind("click");
         $("#gif-container").empty();
         $("#gif-container").removeClass("gif-border");
@@ -31,20 +31,21 @@ function renderButtons (){
 
 // PULL GIF INFO FROM API
 //--------------------------------------------------------------------------------------------------------------
-function populateGifs (show){
+function populateGifs(show) {
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
-		"&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&rating=" + ratingLimit + "&limit=" + gifLimit,
-		method: "GET"
-    }).then(function(response){
+        url: "https://api.giphy.com/v1/gifs/search?q=" + show +
+            "&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&rating=" + ratingLimit + "&limit=" + gifLimit,
+        method: "GET"
+    }).then(function (response) {
 
 
-// CREATE DIV AND CLASS FOR GIFS
-        response.data.forEach(function(element){
+        // CREATE DIV AND CLASS FOR GIFS
+        response.data.forEach(function (element) {
+            console.log(element);
             var newDiv = $("<div>");
             newDiv.addClass("single-gif");
-            newDiv.append("<p>Rating: " + element.rating.toUpperCase()+ "</p>");
-            var newImage = $("<img src= '"+ element.images.fixed_height_still.url + "'>");
+            newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+            var newImage = $("<img src= '" + element.images.fixed_height_still.url + "'>");
             newImage.addClass("gif-image");
             newImage.attr("state", "still");
             newImage.attr("still-data", element.images.fixed_height_still.url);
@@ -53,29 +54,29 @@ function populateGifs (show){
             $("#gif-container").append(newDiv);
         });
 
-// ON CLICK FUNTION TO START AND STOP GIF ANIMATION
-            $("#gif-container").addClass("gif-border");
-            $(".gif-image").unbind("click");
-            $(".gif-image").on("click", function(){
-                if ($(this).attr("state") === "still"){
-                    $(this).attr("state", "animate");
-                    $(this).attr("src", $(this).attr("animate-data"));
-                } else {
-                    $(this).attr("state", "still")
-                    $(this).attr("src", $(this).attr("still-data"));
-                }
-            });
+        // ON CLICK FUNTION TO START AND STOP GIF ANIMATION
+        $("#gif-container").addClass("gif-border");
+        $(".gif-image").unbind("click");
+        $(".gif-image").on("click", function () {
+            if ($(this).attr("state") === "still") {
+                $(this).attr("state", "animate");
+                $(this).attr("src", $(this).attr("animate-data"));
+            } else {
+                $(this).attr("state", "still")
+                $(this).attr("src", $(this).attr("still-data"));
+            }
+        });
 
     });
 }
 
 // CREATE NEW BUTTONS ACCORDING TO WHAT USER INPUTS
-function addButton(show){
-	if(theme.indexOf(show) === -1) {
-		theme.push(show);
-		$("#button-container").empty();
-		renderButtons();
-	}
+function addButton(show) {
+    if (topics.indexOf(show) === -1) {
+        topics.push(show);
+        $("#buttons-container").empty();
+        renderButtons();
+    }
 }
 
 
@@ -83,6 +84,12 @@ function addButton(show){
 // MAIN OPERATIONS
 //--------------------------------------------------------------------------------------------------------------
 
-$(document).ready(function(){
+$(document).ready(function () {
     renderButtons();
+    $("#submit").on("click", function () {
+        event.preventDefault();
+        addButton($("#input-text").val().trim());
+        $("#input-text").val("");
+    })
 });
+
